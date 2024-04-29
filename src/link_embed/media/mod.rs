262517222e443d428ffb3a64bc5caae78b536035
Evan Chang <evanc577@gmail.com>
2024-04-29 10:58:15 -0400

    Use default media link for streamable/imgur

--- src/link_embed/media/mod.rs
+++ src/link_embed/media/mod.rs
@@ -1,20 +1,22 @@
 use self::dubz::DubzMediaResolver;
-use self::imgur::ImgurMediaResolver;
-use self::streamable::StreamableMediaResolver;
 
 mod dubz;
-mod streamable;
-mod imgur;
 
 pub trait MediaLinkResolver {
-    async fn resolve(url: &reqwest::Url) -> Option<Box<str>>;
+    async fn resolve(url: &reqwest::Url) -> Option<Box<str>> {
+        Some(url.as_str().into())
+    }
 }
+
+struct DefaultMediaResolver;
+
+impl MediaLinkResolver for DefaultMediaResolver {}
 
 pub(crate) async fn resolve_media_link(url: &reqwest::Url) -> Option<Box<str>> {
     match url.domain() {
-        Some("streamable.com") => StreamableMediaResolver::resolve(url).await,
         Some("dubz.link") => DubzMediaResolver::resolve(url).await,
-        Some("imgur.com") | Some("i.imgur.com") => ImgurMediaResolver::resolve(url).await,
+        Some("imgur.com") | Some("i.imgur.com") => DefaultMediaResolver::resolve(url).await,
+        Some("streamable.com") => DefaultMediaResolver::resolve(url).await,
         _ => None,
     }
 }
